71f089271dae7ad980208f824dc5d73e842a19f0
unknown <mangulabnanshervinmarco@gmail.com>
2024-04-24 15:15:44 +0700

    added pseudocode

--- python_string_methods.py
+++ python_string_methods.py
@@ -1,1 +1,9 @@
 # Purpose: A program that will allows users to explore and apply different string manipulation techniques in Python.
+
+#Pseudocode
+# Print python string methods 
+# ask a number from the user
+# ask string from the user
+# execute the chosen method 
+# print result
+# ask if the user wants to try again
