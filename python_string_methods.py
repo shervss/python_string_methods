b2b52645bf37e1741dff0f39795f21f698132e5f
unknown <mangulabnanshervinmarco@gmail.com>
2024-04-24 15:28:56 +0700

    print python string methods and their corresponding number

--- python_string_methods.py
+++ python_string_methods.py
@@ -2,6 +2,41 @@
 
 #Pseudocode
 # Print python string methods 
+print ("\nPython String Methods:\n"
+       "1. capitalized() method\n"
+       "2. lstrip() method\n"
+       "3. rstrip() method\n"
+       "4. strip() method\n"
+       "5. lower() method\n"
+       "6. upper() method\n"
+       "7. title() method\n"
+       "8. swapcase() method\n"
+       "9. ljust() method\n"
+       "10. rjust() method\n"
+       "11. center() method\n"
+       "12. zfill() method\n"
+       "13. find() method\n"
+       "14. count() method\n"
+       "15. startswith() method\n"
+       "16. endswith() method\n"
+       "17. isdigit() method\n"
+       "18. isnumeric() method\n"
+       "19. isdecimal() method\n"
+       "20. isalpha() method\n"
+       "21. isalnum() method\n"
+       "22. islower() method\n"
+       "23. isupper() method\n"
+       "24. isspace() method\n"
+       "25. find() method\n"
+       "26. str() method\n"
+       "27. len() method\n"
+       "28. max() method\n"
+       "29. min() method\n"
+       "30. split() method\n"
+       "31. index() method\n"
+       "32. ord() method\n"
+       "33. chr() method\n")
+
 # ask a number from the user
 # ask string from the user
 # execute the chosen method 
